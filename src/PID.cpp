be06cb706432ff29dc1434f235be6c99954fe028
Yuan Li <yuanli1213@Yuans-MacBook-Pro.local>
2017-12-10 17:53:00 -0800

    complete the PID class

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,22 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+
+	p_error = 0.0; //previous CTE
+	i_error = 0.0;
+	d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+	d_error = cte - p_error; 
+	i_error += cte;
+	p_error = cte;
 }
 
 double PID::TotalError() {
+	return i_error;
 }
 
